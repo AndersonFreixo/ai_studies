06b99028472e01727adcc8319a7f869a4776a8bd
linux <linux@debian.db>
2025-11-24 00:36:04 -0300

    add get_weather tool to simple_smolagent

--- simple_smolagent_local.py
+++ simple_smolagent_local.py
@@ -2,15 +2,15 @@
 #Runs locally (ran out of credits at HuggingFace :/) using ollama
 
 from smolagents import ToolCallingAgent, LiteLLMModel, FinalAnswerTool, DuckDuckGoSearchTool
-
+from custom_tools import get_weather
 class App:
     def __init__(self):
          #Agent related
         self.agent = ToolCallingAgent(
-            tools=[FinalAnswerTool(), DuckDuckGoSearchTool()],
-            model = LiteLLMModel(model_id="ollama_chat/qwen2:0.5b",
+            tools=[get_weather, FinalAnswerTool(), DuckDuckGoSearchTool()],
+            model = LiteLLMModel(model_id="ollama_chat/qwen2:7b",
                                  api_base="http://127.0.0.1:11434",
-                                 num_ctx=8192),
+                                 num_ctx=12000),
             )
 
     def run(self):
